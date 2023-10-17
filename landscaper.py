f58918d09442a9b31b99b5be6b96f671436226e2
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-10-17 18:29:59 -0400

    working game

--- landscaper.py
+++ landscaper.py
@@ -1,113 +1,63 @@
-earnings = 0  
-scissors = False  
-lawnmower = False  
-battery_lawnmower = False  
-team_hired = False  
+money = 0
+tools = ["teeth"]
 
-while True:
-    if not scissors:
-        print(f"Current earnings: ${earnings}")
-        buy_choice = input("Do you want to buy a pair of rusty scissors for $5? (yes/no): ").lower()
-        if buy_choice == "yes" and earnings >= 5:
-            earnings -= 5
-            scissors = True
-            print("You bought a pair of rusty scissors!")
-        elif buy_choice == "yes":
-            print("You don't have enough money to buy scissors.")
-        elif buy_choice == "no":
-            pass
+def cut_grass(tool):
+    global money  
+    if tool == "teeth":
+        money += 1
+    elif tool == "scissors":
+        money += 5
+    elif tool == "push lawnmower":
+        money += 50
+    elif tool == "battery-powered lawnmower":
+        money += 100
+    elif tool == "team":
+        money += 250
+
+def buy_tool(tool, cost):
+    global money  
+    if tool not in tools and money >= cost:
+        money -= cost
+        tools.append(tool)
+
+def hire_team():
+    global money  
+    if "team" not in tools and money >= 500:
+        money -= 500
+        tools.append("team")
+
+def get_tool_cost(tool):
+    if tool == "scissors":
+        return 5
+    elif tool == "push lawnmower":
+        return 25
+    elif tool == "battery-powered lawnmower":
+        return 250
+    else:
+        return 0
+    
+
+while money < 1000:
+    print(f"Money: ${money}")
+    print(f"Tools: {', '.join(tools)}")
+
+    action = input("What would you like to do? (cut/buy/hire/quit): ")
+
+    if action == "cut":
+        cut_grass(tools[-1])
+    elif action == "buy":
+        tool_to_buy = input("Which tool would you like to buy? (scissors/push lawnmower/battery-powered lawnmower): ")
+        buy_tool(tool_to_buy, get_tool_cost(tool_to_buy))
+    elif action == "hire":
+        if "team" not in tools:
+            print("You can only hire a team when you have the battery-powered lawnmower.")
         else:
-            print("Please enter a valid choice (yes/no).")
-
-    if scissors:
-        choice = input("Do you want to cut the lawn with your rusty scissors? (yes/no): ").lower()
-        if choice == "yes":
-            earnings += 5  
-            print(f"You earned $5. Your total earnings: ${earnings}")
-        elif choice == "no":
-            pass
-        else:
-            print("Please enter a valid choice (yes/no).")
-
-    if not lawnmower and scissors:
-        print(f"Current earnings: ${earnings}")
-        buy_choice = input("Do you want to buy an old-timey push lawnmower for $25? (yes/no): ").lower()
-        if buy_choice == "yes" and earnings >= 25:
-            earnings -= 25
-            lawnmower = True
-            print("You bought an old-timey push lawnmower!")
-        elif buy_choice == "yes":
-            print("You don't have enough money to buy a lawnmower.")
-        elif buy_choice == "no":
-            pass
-        else:
-            print("Please enter a valid choice (yes/no).")
-
-    if lawnmower:
-        choice = input("Do you want to cut the lawn with your old-timey push lawnmower? (yes/no): ").lower()
-        if choice == "yes":
-            earnings += 50  
-            print(f"You earned $50. Your total earnings: ${earnings}")
-        elif choice == "no":
-            pass
-        else:
-            print("Please enter a valid choice (yes/no).")
-
-    if not battery_lawnmower and lawnmower:
-        print(f"Current earnings: ${earnings}")
-        buy_choice = input("Do you want to buy a fancy battery-powered lawnmower for $250? (yes/no): ").lower()
-        if buy_choice == "yes" and earnings >= 250:
-            earnings -= 250
-            battery_lawnmower = True
-            print("You bought a fancy battery-powered lawnmower!")
-        elif buy_choice == "yes":
-            print("You don't have enough money to buy a battery-powered lawnmower.")
-        elif buy_choice == "no":
-            pass
-        else:
-            print("Please enter a valid choice (yes/no).")
-
-    if battery_lawnmower:
-        choice = input("Do you want to cut the lawn with your fancy battery-powered lawnmower? (yes/no): ").lower()
-        if choice == "yes":
-            earnings += 100  
-            print(f"You earned $100. Your total earnings: ${earnings}")
-        elif choice == "no":
-            pass
-        else:
-            print("Please enter a valid choice (yes/no).")
-
-    if not team_hired and battery_lawnmower:
-        print(f"Current earnings: ${earnings}")
-        hire_choice = input("Do you want to hire a team of starving students for $500? (yes/no): ").lower()
-        if hire_choice == "yes" and earnings >= 500:
-            earnings -= 500
-            team_hired = True
-            print("You hired a team of starving students!")
-        elif hire_choice == "yes":
-            print("You don't have enough money to hire a team.")
-        elif hire_choice == "no":
-            pass
-        else:
-            print("Please enter a valid choice (yes/no).")
-
-    if team_hired:
-        choice = input("Do you want to let your team of starving students cut the lawn? (yes/no): ").lower()
-        if choice == "yes":
-            earnings += 250  
-            print(f"You earned $250. Your total earnings: ${earnings}")
-        elif choice == "no":
-            pass
-        else:
-            print("Please enter a valid choice (yes/no).")
-
-    if earnings >= 1000 and team_hired:
-        print("Congratulations! You've won the game!")
+            hire_team()
+    elif action == "quit":
         break
+    else:
+        print("Invalid choice. Please choose a valid action.")
 
-    choice = input("Do you want to cut the lawn with your teeth? (yes/no): ").lower()
 
-    if choice == "no":
-        break  
-
-print(f"Your total earnings: ${earnings}")
+if money >= 1000:
+    print("Congratulations! You've won the game!")
